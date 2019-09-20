ee3b6c3e0f7f4538648727f709e7a33a7725a27b
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio.local>
2019-09-20 17:10:58 -0300

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+// use App\Http\Controllers\Auth\Auth;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+    /**
+    * Redirect the user to the Facebook authentication page.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function redirectToFacebookProvider()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+
+    /**
+     * Obtain the user information from Facebook.
+     *
+     * @return void
+     */
+    public function handleProviderFacebookCallback()
+    {
+        $auth_user = Socialite::driver('facebook')->user();
+
+        $user = User::updateOrCreate(
+            [
+                'email' => $auth_user->email
+            ],
+            [
+                'token' => $auth_user->token,
+                'name'  =>  $auth_user->name
+            ]
+        );
+
+        Auth::login($user);
+        return redirect()->to('/home'); // Redirect to a secure page
+    }
+
+
+}
